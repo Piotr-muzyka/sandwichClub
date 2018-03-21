da7893f840a1e4c2730580e61343b116d425db2b
Piotr Muzyka <piotrmuzyka@yahoo.com>
2018-03-21 22:33:51 +0100

    Parse JSON Data. Fields output to logger.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,35 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class JsonUtils {
 
+    private static final String TAG = JsonUtils.class.getName();
+
     public static Sandwich parseSandwichJson(String json) {
+
+        try{
+            JSONObject sandwichJSON = new JSONObject(json);
+            JSONObject sandwichName = sandwichJSON.getJSONObject("name");
+
+            String sandwichMainName = sandwichName.getString("mainName");
+            String sandwichAKAName = sandwichName.getString("alsoKnownAs");
+            String sandwichOrigin = sandwichJSON.getString("placeOfOrigin");
+            String sandwichDescription = sandwichJSON.getString("description");
+            String sandwichImage = sandwichJSON.getString("image");
+
+            Log.v(TAG, "Sandwich main name : " + sandwichMainName +" AKA name : " + sandwichAKAName + "Origin : "  + sandwichOrigin + " description : " + sandwichDescription + " image : " + sandwichImage);
+
+        }
+        catch(JSONException e){
+            Log.e( TAG, "Exception", e);
+        }
+
         return null;
     }
 }
